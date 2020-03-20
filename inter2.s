6c27378075468e9016dad6cc9a52e0bfbf792e14
FranciscoVF <60368669+FranciscoVF@users.noreply.github.com>
2020-03-20 15:00:26 -0700

    Create inter2.s

---
+++ inter2.s
@@ -0,0 +1,25 @@
+.include " inter.inc "
+.text
+/* Agrego vector interrupci ón */
+ADDEXC 0x18, irq_handler
+/* Inicializo la pila en modos IRQ y SVC */
+mov r0, # 0b11010010 @ Modo IRQ, FIQ & IRQ desact
+msr cpsr_c, r0
+mov sp, # 0x8000
+mov r0, # 0b11010011 @ Modo SVC, FIQ & IRQ desact
+msr cpsr_c, r0
+mov sp, # 0x8000000
+/* Configuro GPIOs 9, 10, 11, 17, 22 y 27 como salida */
+ldr r0, = GPBASE
+mov r1, # 0b00001000000000000000000000000000
+str r1, [ r0, # GPFSEL0 ]
+/* guia bits xx999888777666555444333222111000 */
+ldr r1, = 0b00000000001000000000000000001001
+str r1, [ r0, # GPFSEL1 ]
+ldr r1, = 0b00000000001000000000000001000000
+str r1, [ r0, # GPFSEL2 ]
+/* Programo contador C1 para dentro de 2 microsegundos */
+ldr r0, = STBASE
+ldr r1, [ r0, # STCLO ]
+add r1, # 2
+str r1, [ r0, # STC1 ]
